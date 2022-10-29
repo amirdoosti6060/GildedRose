65086d04dabecacd2fa4bff00e92943a35fc5fad
Amir <amirdoosti@hotmail.com>
2022-10-29 20:37:35 +0400

    Implement OO approach
    Implement main logic using object oriented and design pattern

---
+++ src/GildedRose.Console/UpdateLogicFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRose.Console
+{
+    internal class UpdateLogicFactory
+    {
+        public IUpdateLogic Create(Item item)
+        {
+            if (item.Name == "Aged Brie")
+                return new AgedBrieUpdateLogic();
+            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                return new BackstageUpdateLogic();
+            else if (item.Name == "Sulfuras, Hand of Ragnaros")
+                return new SulfurasUpdateLogic();
+            else
+                return new GeneralUpdateLogic();
+        }
+    }
+}
